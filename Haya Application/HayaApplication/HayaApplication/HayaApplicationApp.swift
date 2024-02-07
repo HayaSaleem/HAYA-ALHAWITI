b59881239b738d7cf256e45520296b50e21edb9e
HayaSaleem <96831587+HayaSaleem@users.noreply.github.com>
2024-02-07 11:35:00 +0300

    Add files via upload

---
+++ Haya Application/HayaApplication/HayaApplication/HayaApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayaApplicationApp.swift
+//  HayaApplication
+//
+//  Created by Haya Saleem Alhawiti on 13/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HayaApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
